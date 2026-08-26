d503a134d347d050fb592ff6cc292ac26c930afa
SeoHyun1024 <leesophy0805@gmail.com>
2026-08-26 16:31:52 +0900

    0826 실습 : feat : gemini api 연결

--- 0_getstarted.py
+++ 0_getstarted.py
@@ -1,15 +1,30 @@
 from openai import OpenAI
+from google import genai
 
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
-client = OpenAI()
+## open AI API 키
+open_api_key = os.getenv("OPENAI_API_KEY")
+client_0 = OpenAI(api_key=open_api_key)
 
-response = client.responses.create(
+## gemini api 키
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+client_1 = genai.Client()
+
+response_open_api = client_0.responses.create(
     model="gpt-5.6",
     input="Write a one-sentence bedtime story about a unicorn.",
 )
 
-print(response.output_text)
+
+        
+response_gemini = client_1.models.generate_content(
+    model="gemini-3.6-flash", 
+    contents="Explain how AI works in a few words"
+)
+
+print(response_open_api.text) 
+
+print(response_gemini.text)
